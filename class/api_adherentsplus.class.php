24a4e67e8d7c7304c7db3376b5b0d2ca2a1542bd
ptibogxiv <support@ptibogxiv.net>
2018-09-24 22:14:25 +0200

    fix

--- class/api_adherentsplus.class.php
+++ class/api_adherentsplus.class.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2016   Xebax Christy           <xebax@wanadoo.fr>
+/* Copyright (C) 2018   Thibault FOUCART           <support@ptibogxiv.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
