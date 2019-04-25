0415f9e4f895c8c5cb55e1277e378971083ed291
ptibogxiv <support@ptibogxiv.net>
2019-04-26 01:18:51 +0200

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,4 @@
-ALTER TABLE llx_subscription ADD COLUMN fk_type int(11) DEFAULT NULL;
+//ALTER TABLE llx_subscription ADD COLUMN fk_type int(11) DEFAULT NULL;
 
 //ALTER TABLE llx_adherent_type ADD COLUMN morphy varchar(3) DEFAULT NULL;
 
@@ -19,8 +19,6 @@ ALTER TABLE llx_adherent_type ADD COLUMN family int(3) DEFAULT NULL;
 ALTER TABLE llx_adherent_type ADD COLUMN use_default int(11) DEFAULT NULL;
 
 ALTER TABLE llx_adherent ADD COLUMN fk_parent int(11) DEFAULT NULL;
-
-//ALTER TABLE llx_subscription ADD COLUMN fk_type int(11) DEFAULT NULL;
 
 ALTER TABLE llx_adherent ADD COLUMN datecommitment DATE NOT NULL ;
 
