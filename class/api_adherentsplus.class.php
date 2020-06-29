5e748e7e23e59ac71999bde6ea25c11e02ec9345
ptibogxiv <support@ptibogxiv.net>
2020-06-29 20:20:55 +0200

    Update api_adherentsplus.class.php

--- class/api_adherentsplus.class.php
+++ class/api_adherentsplus.class.php
@@ -256,6 +256,7 @@ class AdherentsPlus extends DolibarrApi
             	$obj = $db->fetch_object($result);
                 $membertype = new AdherentTypePlus($this->db);
                 if ($membertype->fetch($obj->rowid)) {
+                    $membertype->subscription_calculator();
                     $obj_ret[] = $this->_cleanObjectDatas($membertype);
                 }
                 $i++;
