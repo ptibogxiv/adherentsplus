ecd08aedced4e90fe5fdee7d7323b4871154f361
ptibogxiv <support@ptibogxiv.net>
2020-06-30 15:24:10 +0200

    wip

--- admin/adherent.php
+++ admin/adherent.php
@@ -265,7 +265,7 @@ print "</td></tr>\n";
 // type of adhesion flow
 print '<tr class="oddeven"><td>'.$langs->trans("ADHERENT_SUBSCRIPTION_PRORATA").'</td>';
 print '<td>';
-print $form->selectarray('ADHERENT_SUBSCRIPTION_PRORATA', array('0'=>$langs->trans("ADHERENT_SUBSCRIBE_NO_MONTH_START"),'1'=>$langs->trans("ADHERENT_SUBSCRIPTION_ANNUAL")), (empty($conf->global->ADHERENT_SUBSCRIPTION_PRORATA)?'0':$conf->global->ADHERENT_SUBSCRIPTION_PRORATA), 0);
+print $form->selectarray('ADHERENT_SUBSCRIPTION_PRORATA', array('0'=>$langs->trans("ADHERENT_PRORATA_SUBSCRIPTION_FREE"),'1'=>$langs->trans("ADHERENT_PRORATA_SUBSCRIPTION_BLOCK"),'2'=>$langs->trans("ADHERENT_PRORATA_SUBSCRIPTION_TYPE")), (empty($conf->global->ADHERENT_SUBSCRIPTION_PRORATA)?'0':$conf->global->ADHERENT_SUBSCRIPTION_PRORATA), 0);
 print '</td>';
 print "</tr>\n";
 
