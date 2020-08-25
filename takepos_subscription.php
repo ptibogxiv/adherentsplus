b8eb7818962c39f3d8ef5b81609b4087fba78a5a
ptibogxiv <support@ptibogxiv.net>
2020-08-25 21:57:18 +0200

    Update takepos_subscription.php

--- takepos_subscription.php
+++ takepos_subscription.php
@@ -239,20 +239,21 @@ echo $langs->trans("None");
       $membertype = new AdherentTypePlus($db); 
       $membertype->fetch($objp->rowid);
       $membertype->subscription_calculator($adh->id);
-//if (($adh->datefin <= dol_now()) || ($membertype->date_renew <= dol_now()) || ($adh->datefin < $membertype->date_renew)) {     
+//if (($adh->datefin <= dol_now()) || ($membertype->date_renew <= dol_now()) || ($adh->datefin < $membertype->date_renew)) { 
+if (empty($membertype->morphy) || ($membertype->morphy == $adh->morphy)) {    
 print '<button type="button" class="';
 if ($membertype->id == $adh->typeid) { 
 print "calcbutton poscolorblue";
 } else {
 print "calcbutton poscolordelete";
 }
-print '" onclick="location.href=\'takepos_subscription.php?action=change&idmember='.$adh->id.'&type='.$membertype->id.'&invoiceid='.$invoiceid.'&place='.urlencode($place).'\'">'.dol_escape_htmltag($membertype->label).'<br><small>';
+print '" onclick="location.href=\'takepos_subscription.php?action=change&idmember='.$adh->id.'&type='.$membertype->id.'&invoiceid='.$invoiceid.'&place='.urlencode($place).'\'" >'.dol_escape_htmltag($membertype->label).'<br><small>';
 print '('.price($membertype->price_prorata).' '.$langs->trans("Currency".$conf->currency);
 if ($membertype->price_prorata != $membertype->nextprice) { print ' '.$langs->trans("then").' '.price($membertype->nextprice).' '.$langs->trans("Currency".$conf->currency); }
 print ')<br>';
 print ''.dol_print_date($membertype->date_begin, 'day').' - '.dol_print_date($membertype->date_end, 'day');
 print '</small></button>';
-//}
+}
 			$i++;
 		}
 	}
