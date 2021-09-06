046bbfa0b42525439b2ee8dc39b480eb0ef96b9d
ptibogxiv <support@ptibogxiv.net>
2021-09-06 19:58:44 +0200

    Create update_13.0.0-14.0.0.sql

---
+++ sql/update_13.0.0-14.0.0.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  quentin
+ * Created: 2 oct. 2018
+ */
+
+UPDATE llx_adherent_type as A SET amount= (SELECT B.price FROM llx_adherent_type as B WHERE A.rowid=B.rowid);
+ALTER TABLE llx_adherent_type DROP COLUMN price;
