d7badb8735857d0e703496b0ba2a906b46a209b4
ptibogxiv <support@ptibogxiv.net>
2021-12-28 13:57:00 +0100

    Update takepos_subscription.php

--- takepos_subscription.php
+++ takepos_subscription.php
@@ -21,16 +21,6 @@
  *	\brief      Page with the content of the popup to enter payments
  */
 
-//if (! defined('NOREQUIREUSER'))	define('NOREQUIREUSER', '1');	// Not disabled cause need to load personalized language
-//if (! defined('NOREQUIREDB'))		define('NOREQUIREDB', '1');		// Not disabled cause need to load personalized language
-//if (! defined('NOREQUIRESOC'))		define('NOREQUIRESOC', '1');
-//if (! defined('NOREQUIRETRAN'))		define('NOREQUIRETRAN', '1');
-if (!defined('NOCSRFCHECK'))		define('NOCSRFCHECK', '1');
-if (!defined('NOTOKENRENEWAL'))	define('NOTOKENRENEWAL', '1');
-if (!defined('NOREQUIREMENU'))		define('NOREQUIREMENU', '1');
-if (!defined('NOREQUIREHTML'))		define('NOREQUIREHTML', '1');
-if (!defined('NOREQUIREAJAX'))		define('NOREQUIREAJAX', '1');
-
 $res = 0;
 if (! $res && file_exists("../main.inc.php"))
 {
@@ -66,8 +56,10 @@ $invoiceid = GETPOST('invoiceid', 'int');
 $type = GETPOST('type', 'int');
 
 if (empty($user->rights->takepos->run)) {
-	accessforbidden();
-}
+	$message = null;
+	if ($constforcompanyid == $invoice->socid || empty($invoice->socid))  $message = $langs->trans('MembershipNotAllowedForGenericCustomer');
+		accessforbidden($message);
+	}
 
 $constforcompanyid = $conf->global->{'CASHDESK_ID_THIRDPARTY'.$_SESSION["takeposterminal"]};
 
